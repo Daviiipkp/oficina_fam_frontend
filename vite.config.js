88ecbe7640a5787b0fa971869d53c41dc86ce7fa
Daviiipkp <daviipkp@outlook.com>
2026-05-27 08:48:21 +0200

    added ambients

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/absproxy/5173/', 
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['code.daviipkp.org'],
-    port: 5173,
-    hmr: {
-      protocol: 'wss',
-      clientPort: 443, 
-      path: '/absproxy/5173/', 
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  return {
+    base: isDev ? '/absproxy/5173/' : '/', 
+    
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: ['code.daviipkp.org'],
+      port: 5173,
+      hmr: {
+        protocol: 'wss',
+        clientPort: 443, 
+        path: '/absproxy/5173/', 
+      }
     }
   }
 })
