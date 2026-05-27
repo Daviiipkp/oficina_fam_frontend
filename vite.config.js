d9812c775907b7a9d141d012df078abe0777c983
Daviiipkp <daviipkp@outlook.com>
2026-05-27 08:10:02 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  base: '/absproxy/5173/', 
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['code.daviipkp.org'],
+    port: 5173,
+    hmr: {
+      protocol: 'wss',
+      clientPort: 443, 
+      path: '/absproxy/5173/', 
+    }
+  }
+})
